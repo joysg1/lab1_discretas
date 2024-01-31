c9dc439f51ca10105730cc50b20207d7b4acaf16
joysg1 <joy.nelaton@utp.ac.pa>
2024-01-31 16:46:33 -0500

    Commit_1

---
+++ prob.py
@@ -0,0 +1,86 @@
+import math
+
+U = set(range(-1000000, 1000001))
+
+# print(len(U))
+
+
+A = set([1,2,3,4])
+
+u = math.pow(2,65)
+v = math.pow(2,66)
+w = math.pow(2,67) 
+
+G = {u,v,w}
+
+#print(G<=A)
+
+#print(len(G))
+
+#print(len(A))
+#print(4 in A)
+
+
+B = set([-2,-1,0])
+# print(len(B))
+
+# vacio = set()
+
+# print(vacio<=B)
+
+
+C = {-1.30, -1, 0, 1, 2, 2.30}
+# print(len(C))
+
+D = {0,1,2}
+# print(len(D))
+
+E = {-9.24, -8, -7, -6, -5, -4, -3, -2, -1, 0, 9.74, 8, 7,6,5,4,3,2,1}
+
+# print(len(E))
+
+F = {5, "-2.50 +4.21i", "-2.50 -4.21i"}
+
+# print(F<=E)
+
+# print(len(F))
+
+# print(F-E)
+# print(C-E)
+# print(U-E)
+
+
+#print (A | B)
+
+#print (B | A)
+
+#print (D | A)
+
+
+
+#print(A&D)
+
+#print(A-B)
+
+#print(B ^ C)
+#print(B)
+#print(C)
+
+#print((B|C) - (B&C))
+
+
+#print(D - (C-B))
+
+
+#print((E^A)-B)
+
+             
+                
+
+
+
+
+
+
+
+
